be7f224cd236ac9fb663db276c349e02d652ee43
webheadthwip <dannytran3@gmail.com>
2021-06-02 00:21:50 -0400

    added typography

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import './style.css'
 import 'virtual:windi.css'
+import './style.css'
 import darkMode from './js/dark-mode';
 
 darkMode();
--- windi.config.js
+++ windi.config.js
@@ -4,14 +4,30 @@ export default defineConfig({
   /* configurations... */
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      'sans': ['Public Sans', system - ui, -apple - system, BlinkMacSystemFont,
+        'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans - serif,
+        'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      'mono': ['Office Code Pro', Menlo, Monaco, Consolas, 'Liberation Mono',
+        'Courier New', monospace]
+    },
+    fontSize: {
+      'sm': '.8rem',
+      'base': '1rem',
+      'lg': '1.25rem',
+      'xl': '1.563rem',
+      '2xl': '1.953rem',
+      '3xl': '2.441rem',
+      '4xl': '4.052rem',
+    },
       colors: {
-        black: '#1a1a1a',
-        white: '#eff5f4',
-        teal: {
-            DEFAULT: '#43dbb2',
-            dark: '#1f2e2a',
-        }, 
-        red: '#ff476f',
-      }
+      black: '#1a1a1a',
+      white: '#eff5f4',
+      teal: {
+        DEFAULT: '#43dbb2',
+        dark: '#1f2e2a',
+      },
+      red: '#ff476f',
+    }
   }
 })
