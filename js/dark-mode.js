1fb7b29aa2bd3e52bdc62e08093617d613190bdd
webheadthwip <dannytran3@gmail.com>
2021-06-20 03:58:20 -0400

    fixed colors

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -18,6 +18,7 @@ export default function darkMode() {
         disableDarkMode();
     }
 
+    if(darkModeToggle){
     darkModeToggle.addEventListener('click', () => {
         darkMode = localStorage.getItem('darkMode');
         if (darkMode !== 'enabled') {
@@ -25,5 +26,5 @@ export default function darkMode() {
         } else {
             disableDarkMode();
         }
-    });
+    });}
 }
--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import 'virtual:windi.css'
 import './styles/styles.scss'
-import darkMode from './js/dark-mode';
 import modal from './js/modals';
 
-darkMode();
 modal();
